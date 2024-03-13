dd987f3dd53f28d5242f55106f2d60d678a1f5a7
FIRST_NAME <billuramya2002@gamil.com>
2024-03-13 10:32:38 +0530

    FileHandling

---
+++ FileHandling/FlieHandlingProblems.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileHandling
+{
+    //how to write all lines of a text
+    public class FlieHandlingProblems
+    {
+        public void WriteText()
+        {
+            string filePath = @"C:\Users\billu\source\repos\FileHandling\text.txt";
+            try
+            {
+                string data = "Hello World";
+                File.WriteAllText(filePath, data);
+            }
+            catch(IOException e) {
+                Console.WriteLine(e.Message);
+            }
+           
+
+        }
+        //read all line of text 
+        public void ReadText()
+        {
+            string path = @"C:\Users\billu\source\repos\FileHandling\text.txt";
+            try
+            {
+                string[] lines = File.ReadAllLines(path);
+                foreach (string line in lines)
+                {
+                    Console.WriteLine(line);
+                }
+            }catch(IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+        //How to append data to an existing text file 
+        public void AppendOfExistingFile()
+        {
+            string path= @"C:\Users\billu\source\repos\FileHandling\text.txt";
+            string data = "this is DotNet"; 
+
+        }
+    }
+}
---
+++ FileHandling/Program.cs
@@ -0,0 +1,11 @@
+﻿using FileHandling;
+using System;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        FlieHandlingProblems file = new FlieHandlingProblems();
+        file.WriteText();
+        file.ReadText();
+    }
+}
